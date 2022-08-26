6863ff5eeac71ee9816f0ca2bd23d8fc87cda1ec
jyotiDeshmukh1988 <83502437+jyotiDeshmukh1988@users.noreply.github.com>
2022-08-25 19:20:19 -0700

    add cuisinefilter and price filter

--- zomapp/src/component/booking/placeOrder.js
+++ zomapp/src/component/booking/placeOrder.js
@@ -1,13 +1,14 @@
 import React,{Component} from 'react';
 import './placeOrder.css';
 
-const url = "http://zomatoajulypi.herokuapp.com/menuItem"; // get all the selected add to cart items
-const placeOrder = ""
+const url = "http://zomatoajulypi.herokuapp.com/menuItem";
+const placeOrder = "http://localhost:9870/orders"
 
-class PlaceOrder extends Component {
+class PlaceOrder extends Component{
 
     constructor(props){
-        super(props);
+        super(props)
+
         this.state={
             id:Math.floor(Math.random()*100000),
             hotel_name:this.props.match.params.restName,
@@ -20,41 +21,114 @@ class PlaceOrder extends Component {
         }
     }
 
-    render(){
-        return (
-            <div className="container">
-            <div className="panel panel-primary">
-                <div className="panel-heading">
-                 <h3>Your Order Form Restaurant for the {this.state.hotel_name}</h3>
-                </div>
-                <div className="panel-body">
-                    <form>
-                        <div className="form-group col-md-6">
-                            <label>Name</label>
-                            <input className="form-control" name="name"
-                            value={this.state.name}/>
-                        </div>
-                        <div className="form-group col-md-6">
-                            <label>Email</label>
-                            <input className="form-control" name="email"
-                            value={this.state.email}/>
-                        </div>
-                        <div className="form-group col-md-6">
-                            <label>Phone</label>
-                            <input className="form-control" name="phone"
-                            value={this.state.phone}/>
-                        </div>
-                        <div className="form-group col-md-6">
-                            <label>Address</label>
-                            <input className="form-control" name="address"
-                            value={this.state.address}/>
-                        </div>
+    handleChange = (event) => {
+        this.setState({[event.target.name]:event.target.value})
+    }
 
-                    </form>
-                </div>
+    handleCheckout = () => {
+        let obj = this.state;
+        obj.menuItem = sessionStorage.getItem('menu');
+        console.log("myobj"+obj.menuItem);
+        fetch(placeOrder,{
+            method:'POST',
+            headers: {
+                'accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(obj)
+        })
+        .then(this.props.history.push(`/viewBooking`))
+    }
+
+    renderItem = (data) => {
+        if(data){
+            return data.map((item) => {
+                return(
+                    <div className="orderItems" key={item.menu_id}>
+                        <img src={item.menu_image} alt={item.menu_name}/>
+                        <h3>{item.menu_name}</h3>
+                        <h4>Rs. {item.menu_price}</h4>
+                    </div>
+                )
+            })
+        }
+    }
+
+    render(){
+        return(
+           <div className="container">
+               <div className="panel panel-primary">
+                   <div className="panel-heading">
+                    <h3>Your Order Form Restaurant {this.state.hotel_name}</h3>
+                   </div>
+                   <div className="panel-body">
+                       <form>
+                           <div className="row">
+                               <input type="hidden" name="cost" value={this.state.cost}/>
+                               <input type="hidden" name="id" value={this.state.id}/>
+                               <input type="hidden" name="hotel_name" value={this.state.hotel_name}/>
+                                <div className="form-group col-md-6">
+                                    <label>Name</label>
+                                    <input className="form-control" name="name"
+                                    value={this.state.name} onChange={this.handleChange}/>
+                                </div>
+                                <div className="form-group col-md-6">
+                                    <label>Email</label>
+                                    <input className="form-control" name="email"
+                                    value={this.state.email} onChange={this.handleChange}/>
+                                </div>
+                                <div className="form-group col-md-6">
+                                    <label>Phone</label>
+                                    <input className="form-control" name="phone"
+                                    value={this.state.phone} onChange={this.handleChange}/>
+                                </div>
+                                <div className="form-group col-md-6">
+                                    <label>Address</label>
+                                    <input className="form-control" name="address"
+                                    value={this.state.address} onChange={this.handleChange}/>
+                                </div>
+                           </div>
+                           {this.renderItem(this.state.menuItem)}
+                           <div className="row">
+                               <div className="col-md-12">
+                                   <h2>Total Price is Rs. {this.state.cost}</h2>
+                               </div>
+                           </div>
+                           <button className="btn btn-success" onClick={this.handleCheckout} >Checkout</button>
+                       </form>
+                   </div>
+               </div>
             </div>
-         </div>
         )
+    }
+
+    //calling api 
+    componentDidMount(){
+        let menuItem = sessionStorage.getItem('menu');
+        console.log("mymenu"+menuItem);
+        let orderId = [];
+        menuItem.split(',').map((item) => {
+            orderId.push(parseInt(item));
+            return 'ok'
+        })
+        fetch(url,{
+            method: 'POST',
+            headers:{
+                'accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(orderId)
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            //console.log(data)
+            let totalPrice = 0;
+            data.map((item) => {
+                totalPrice = totalPrice + Number(item.menu_price);
+                return 'ok'
+            })
+            this.setState({menuItem:data,cost:totalPrice});
+        })
     }
 }
 
